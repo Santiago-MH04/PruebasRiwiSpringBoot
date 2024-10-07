5aec3f2ae7add72c40f96ae788978af042e658e1
Santiago <santimh04@gmail.com>
2024-10-07 00:17:30 -0500

    feature: Swagger dependency and destination file SwaggerConfig created

---
+++ src/main/java/org/springboot/jpa/riwi/pruebas/utils/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package org.springboot.jpa.riwi.pruebas.utils;
+
+public class SwaggerConfig {
+    //Atributos de SwaggerConfig
+    //Constructores de SwaggerConfig
+    //Asignadores de atributos de SwaggerConfig (setters)
+    //Lectores de atributos de SwaggerConfig (getters)
+    //Métodos de SwaggerConfig
+}
